933c83080286ebd41fa276cc990dbe7e4733824b
goooog <690825029@qq.com>
2017-03-25 20:22:47 +0800

    modify

---
+++ bxs/variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+required_vars=('years','lingqu','duration','smoke','social','plan')
+var_fixed_range={
+	'smoke':[0,1],
+	'lingquType':['yd1','yd2'],
+	'age':range(0,81),
+	'sex':[1,2],
+	'social':[0,1],
+	'plan':range(1,6)
+}
+
